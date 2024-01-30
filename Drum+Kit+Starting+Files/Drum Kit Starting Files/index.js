f90eb992648a46ccf0c44b2c0f2bf8a1f6600714
reimo22 <reimo1412@mail.com>
2024-01-30 16:44:03 +0800

    insert background, play audio

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -2,6 +2,10 @@ var buttons = ["w", "a", "s", "d", "j", "k", "l"];
 for (let i = 0; i < buttons.length; i++)
   [document.querySelector(`.${buttons[i]}`).addEventListener("click", clickMe)];
 
+  var audio = new Audio("./sounds/tom-1.mp3");
+
 function clickMe() {
-  alert("You clicked me");
+  audio.play();
 }
+
+
